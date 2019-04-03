039666deb35be605e2e86e5448a82bc83e031a21
Maurizio Tomasi <ziotom78@gmail.com>
2019-04-03 09:08:23 +0200

    Modularize the implementation of genpointings

--- src/CMBMissionSim.jl
+++ src/CMBMissionSim.jl
@@ -18,24 +18,108 @@ export MINUTES_PER_DAY,
        rpm2angfreq,
        period2rpm,
        genpointings,
-       dipoletemperature
+       dipoletemperature,
+       PointingInfo,
+       time2pointing
 
 """
     rpm2angfreq(v)
 
 Convert rotations per minute into angular frequency 2πν (in Hertz).
 """
-rpm2angfreq(v) = 2 * π * v / 60.0
+rpm2angfreq(v) = 2π * v / 60
 
 """
     period2rpm(p)
 
 Convert a period (time span) in seconds into a number of rotations per minute
 """
-period2rpm(p) = 60.0 / p
+period2rpm(p) = 60 / p
 
 const MINUTES_PER_DAY = 60 * 24
 const DAYS_PER_YEAR = 365.25
+
+struct PointingInfo
+    ω_spin::Float64
+    ω_prec::Float64
+    ω_year::Float64
+    ω_hwp::Float64
+    spinsunang::Float64
+    borespinang::Float64
+    # First quaternion used in the rotation
+    q1::Quaternions.Quaternion
+    # Third quaternion used in the rotation
+    q3::Quaternions.Quaternion
+    
+    PointingInfo(
+        spin_rpm=0,
+        prec_rpm=0,
+        yearly_rpm = 1  / (MINUTES_PER_DAY * DAYS_PER_YEAR),
+        hwp_rpm = 0,
+        spinsunang_rad = deg2rad(45.0),
+        borespinang_rad = deg2rad(50.0),
+    ) = new(
+        rpm2angfreq(spin_rpm),
+        rpm2angfreq(prec_rpm),
+        rpm2angfreq(yearly_rpm),
+        rpm2angfreq(hwp_rpm),
+        spinsunang_rad,
+        borespinang_rad,
+        Quaternions.qrotation(Float64[0, 1, 0], borespinang_rad),
+        Quaternions.qrotation(Float64[0.0, 1.0, 0.0], π / 2 - spinsunang_rad)
+    )
+end
+
+function time2pointing(pinfo::PointingInfo, time_s, dir, polangle)
+    curpolang = mod2pi(polangle + 4 * pinfo.ω_hwp * time_s)
+    # The polarization vector lies on the XY plane; if polangle=0 then
+    # the vector points along the X direction at t=0.
+    poldir = SVector(cos(curpolang), sin(curpolang), 0.0)
+    
+    q2 = Quaternions.qrotation(Float64[0.0, 0.0, 1.0], pinfo.ω_spin * time_s)
+    q4 = Quaternions.qrotation(Float64[1.0, 0.0, 0.0], pinfo.ω_prec * time_s)
+    q5 = Quaternions.qrotation(Float64[0.0, 0.0, 1.0], pinfo.ω_year * time_s)
+    
+    qtot = q5 * (q4 * (pinfo.q3 * (q2 * pinfo.q1)))
+    rot = Quaternions.rotationmatrix(qtot)
+    # Direction in the sky of the beam main axis
+    curvec = rot * dir
+    # Direction in the sky of the beam polarization axis
+    poldir = rot * poldir
+    
+    # The North for a vector v is just -dv/dθ, as θ is the
+    # colatitude and moves along the meridian
+    (θ, ϕ) = Healpix.vec2ang(curvec...)
+    northdir = SVector(-cos(θ) * cos(ϕ), -cos(θ) * sin(ϕ), sin(θ))
+    
+    cosψ = clamp(dot(northdir, poldir), -1, 1)
+    crosspr = northdir × poldir
+    sinψ = clamp(sqrt(dot(crosspr, crosspr)), -1, 1)
+    ψ = atan(cosψ, sinψ)
+
+    (θ, ϕ, ψ, curvec...)
+end
+
+function genpointings(pinfo::PointingInfo, timerange_s, dir, polangle; usedirs=true)
+    dirs = Array{Float64}(undef, length(timerange_s), usedirs ? 2 : 3)
+    ψ = Array{Float64}(undef, length(timerange_s))
+
+    @inbounds for (idx, t) in enumerate(timerange_s)
+        curθ, curϕ, curψ, curx, cury, curz = time2pointing(pinfo, t, dir, polangle)
+
+        if usedirs
+            dirs[idx, 1] = curθ
+            dirs[idx, 2] = curϕ
+        else
+            dirs[idx, 1] = curx
+            dirs[idx, 2] = cury
+            dirs[idx, 3] = curz
+        end
+        ψ[idx] = curψ
+    end
+
+    dirs
+end
 
 function genpointings!(timerange_s, dir, polangle, dirs, ψ;
     spinsunang=deg2rad(45.0),
@@ -51,8 +135,9 @@ function genpointings!(timerange_s, dir, polangle, dirs, ψ;
     ω_year = rpm2angfreq(yearlyrpm)
     ω_hwp = rpm2angfreq(hwprpm)
     
-    # This quaternion never changes
+    # These quaternions never change
     q1 = Quaternions.qrotation(Float64[0, 1, 0], borespinang)
+    q3 = Quaternions.qrotation(Float64[0.0, 1.0, 0.0], π / 2 - spinsunang)
 
     @inbounds for (idx, time_s) in enumerate(timerange_s)
         curpolang = mod2pi(polangle + 4 * ω_hwp * time_s)
@@ -61,7 +146,6 @@ function genpointings!(timerange_s, dir, polangle, dirs, ψ;
         poldir = SVector(cos(curpolang), sin(curpolang), 0.0)
         
         q2 = Quaternions.qrotation(Float64[0.0, 0.0, 1.0], ω_spin * time_s)
-        q3 = Quaternions.qrotation(Float64[0.0, 1.0, 0.0], π / 2 - spinsunang)
         q4 = Quaternions.qrotation(Float64[1.0, 0.0, 0.0], ω_prec * time_s)
         q5 = Quaternions.qrotation(Float64[0.0, 0.0, 1.0], ω_year * time_s)
         
